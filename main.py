911ec59c877d981422424683c6f21ee810692ed9
Replit user <>
2023-02-09 14:41:22 +0000

    practice for 2nd days

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello"+input("What's your name?"))
+#print("Hello"+input("What's your name?"))
+
+#print(len(12345))
+
+#print("Hello"[4])
+
+#num_char = len(input("what's your name?"))
+#new_num_char = str(num_char)
+#print("Your name has "+ new_num_char +" characters.wooo")
+
+#int(str) float(str) str(int)
+
+#print (type(6/3))
+#print(2**3)
+
